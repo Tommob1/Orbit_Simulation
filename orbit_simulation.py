7d952dd3f33b42556d743bd3124a68214f5ca18e
Brayden Tomlinson <humortronic@gmail.com>
2024-06-25 13:35:22 +0930

    Added changes to code

--- orbit_simulation.py
+++ orbit_simulation.py
@@ -2,34 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-G = 6.67430e-11
-m1 = 1.989e30
-m2 = 5.972e24
-r1 = np.array([0.0, 0.0], dtype=float)
-r2 = np.array([1.496e11, 0.0], dtype=float)
-v1 = np.array([0.0, 0.0], dtype=float)
-v2 = np.array([0.0, 29780.0], dtype=float)
+# Constants
+G = 6.67430e-11  # Gravitational constant
+m1 = 1.989e30    # Mass of the sun
+m2 = 5.972e24    # Mass of the earth
 
-dt = 60 * 60 * 60
-t_max = 365.25 * 24 * 60 * 60
+# Initial positions and velocities
+r1 = np.array([0.0, 0.0], dtype=float)         # Sun's initial position
+r2 = np.array([1.496e11, 0.0], dtype=float)    # Earth's initial position
+v1 = np.array([0.0, 0.0], dtype=float)         # Sun's initial velocity
+v2 = np.array([0.0, 29780.0], dtype=float)     # Earth's initial velocity
 
+# Time settings
+dt = 60 * 60      # Time step in seconds (1 hour)
+t_max = 365.25 * 24 * 60 * 60  # One year in seconds
+
+# Lists to store positions
 positions1 = []
 positions2 = []
 
-t = 0
-
-while t < t_max:
+# Function to compute gravitational force
+def compute_gravitational_force(r1, r2, m1, m2):
     r = r2 - r1
     distance = np.linalg.norm(r)
-
     force = G * m1 * m2 / distance**2
     force_vector = force * r / distance
+    return force_vector
 
+# Simulation loop
+t = 0
+while t < t_max:
+    force_vector = compute_gravitational_force(r1, r2, m1, m2)
+
+    # Update velocities
     v1 += force_vector / m1 * dt
     v2 -= force_vector / m2 * dt
+
+    # Update positions
     r1 += v1 * dt
     r2 += v2 * dt
 
+    # Store positions
     positions1.append(r1.copy())
     positions2.append(r2.copy())
 
@@ -38,6 +51,7 @@ while t < t_max:
 positions1 = np.array(positions1)
 positions2 = np.array(positions2)
 
+# Plotting
 fig, ax = plt.subplots(figsize=(8, 8))
 ax.set_xlim(-1.6e11, 1.6e11)
 ax.set_ylim(-1.6e11, 1.6e11)
@@ -47,19 +61,33 @@ line2, = ax.plot([], [], lw=2, label='Body 2 Path', color='blue')
 point1, = ax.plot([], [], 'o', color='orange', label='Body 1')
 point2, = ax.plot([], [], 'o', color='blue', label='Body 2')
 
+# Text annotations for telemetry data
+telemetry_text1 = ax.text(0.02, 0.95, '', transform=ax.transAxes, color='orange')
+telemetry_text2 = ax.text(0.02, 0.90, '', transform=ax.transAxes, color='blue')
+
 def init():
     line1.set_data([], [])
     line2.set_data([], [])
     point1.set_data([], [])
     point2.set_data([], [])
-    return line1, line2, point1, point2
+    telemetry_text1.set_text('')
+    telemetry_text2.set_text('')
+    return line1, line2, point1, point2, telemetry_text1, telemetry_text2
 
 def animate(i):
     line1.set_data(positions1[:i, 0], positions1[:i, 1])
     line2.set_data(positions2[:i, 0], positions2[:i, 1])
     point1.set_data(positions1[i, 0], positions1[i, 1])
     point2.set_data(positions2[i, 0], positions2[i, 1])
-    return line1, line2, point1, point2
+
+    telemetry_text1.set_text(
+        f'Body 1 (Sun)\nPosition: ({positions1[i, 0]:.2e}, {positions1[i, 1]:.2e})\nVelocity: ({v1[0]:.2e}, {v1[1]:.2e})'
+    )
+    telemetry_text2.set_text(
+        f'Body 2 (Earth)\nPosition: ({positions2[i, 0]:.2e}, {positions2[i, 1]:.2e})\nVelocity: ({v2[0]:.2e}, {v2[1]:.2e})'
+    )
+
+    return line1, line2, point1, point2, telemetry_text1, telemetry_text2
 
 anim = animation.FuncAnimation(fig, animate, init_func=init,
                                frames=len(positions1), interval=20, blit=True)
